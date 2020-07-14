c2ebdee3c15d8c55351da62f081c8a643cd91d1b
Yash Jindal <yash.jindal@razorpay.com>
2020-07-14 18:28:27 +0530

    added pull request

--- action.js
+++ action.js
@@ -6,6 +6,7 @@ const issueIdRegEx = /([a-zA-Z0-9]+-[0-9]+)/g
 const eventTemplates = {
   branch: '{{event.ref}}',
   commits: "{{event.commits.map(c=>c.message).join(' ')}}",
+  pullRequest : "{{github.event.pull_request.title}}",
 }
 
 module.exports = class {
